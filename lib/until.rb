9c2715b0ec980281ef0199d447b73e5981b3032e
Nausheen Akhter <nausheen701@gmail.com>
2020-11-16 04:18:14 +0300

    commit tree f74bd5b4ce2250e4aa35b2555c280b4f300b7ece

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
-def using_until
+def using_until(spell = "Wingardium Leviosa")
   levitation_force = 6
-  #your code here
-    
+  until levitation_force == 10
+  puts "#{spell}"
+  levitation_force += 1
+  end
+  #your code here   
 end
 
