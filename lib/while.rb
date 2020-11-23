31667e6204ac8c3d2798878c4ee9b910d72632c4
Nausheen Akhter <nausheen701@gmail.com>
2020-11-23 03:40:13 +0300

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,9 @@
-def using_while
+def using_while(spell = "Wingardium Leviosa")
   levitation_force = 6
-	
+  while levitation_force < 10
+  puts "#{spell}"
+  levitation_force += 1
+  end
   #your code here
 end
 
